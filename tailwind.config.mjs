b2c2730b2a71adbff9179eb1660aeb209146565d
samawong <samafed@gmail.com>
2025-01-13 10:31:18 +0800

    modify home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+	   extend: {
+		keyframes: {
+            'firefly-move': {
+              '0%': {transform: 'translate(0, 0)'},
+               '50%': {transform: 'translate(20px, 20px)'},
+               '100%': {transform: 'translate(0, 0)'},
+         },
+         'firefly-flicker': {
+                 '0%,100%': {opacity: '0.6'},
+               '50%': {opacity: '0.2'},
+          }
+          },
+          animation: {
+              'firefly-move': 'firefly-move 2s infinite linear',
+            'firefly-flicker': 'firefly-flicker 1s infinite linear'
+          }
+		},
+	   },
 	plugins: [],
 }
